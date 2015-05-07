88469f22abcdfe2f8e5da62c3e666763db2a6a79
Petar Ivanov <pepincho95@gmail.com>
2015-05-07 20:49:04 +0300

    adding some more functions to the structure

---
+++ magic_reservation_system.py
@@ -0,0 +1,25 @@
+class CommandInterface:
+
+    @staticmethod
+    def show_movies():
+        pass
+
+    @staticmethod
+    def show_movie_projections(movie_id, date):
+        pass
+
+    @staticmethod
+    def make_reservation():
+        pass
+
+    @staticmethod
+    def cancel_reservation():
+        pass
+
+    @staticmethod
+    def exit():
+        pass
+
+    @staticmethod
+    def help():
+        pass
